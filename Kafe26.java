08decc7edf2d430f4a38b09e1a20380a1150ee7e
sarahgeiza <sarahgeizask@gmail.com>
2025-11-27 15:30:25 +0700

    Percobaan 1 Pertanyaan 2

--- Kafe26.java
+++ Kafe26.java
@@ -1,5 +1,5 @@
 public class Kafe26 {
-    public static void Menu() {
+    public static void main(String[] args) {
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappucino - Rp 20,000");
@@ -10,8 +10,5 @@ public class Kafe26 {
         System.out.println("===========================");
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
     }
-
-    public static void main(String[] args) {
-        Menu();
-    }
 }
+

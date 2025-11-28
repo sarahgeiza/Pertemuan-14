91ca7ec66a878ac9abaed214c222acf0b7787aef
sarahgeiza <sarahgeizask@gmail.com>
2025-11-28 20:55:03 +0700

    Percobaan 4 Pertanyaan 2

--- PengunjungCafe26.java
+++ PengunjungCafe26.java
@@ -1,11 +1,10 @@
 public class PengunjungCafe26 {
     public static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung:");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
         }
     }
-
     public static void main(String[] args) {
         daftarPengunjung("Ali", "Budi", "Citra");
         daftarPengunjung("Andi");
